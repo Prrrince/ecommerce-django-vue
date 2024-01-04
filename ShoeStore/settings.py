db7c19fe7cf19fb7267d581604b673c77d022b6a
AmarjotSingh21 <amarjot.singh070@gmail.com>
2024-01-04 21:35:23 +0530

    updated allowed hosts

--- ShoeStore/settings.py
+++ ShoeStore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ['my-shoestore.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['shoestore.amarjot.dev', 'localhost']
 
 AUTH_USER_MODEL = 'user.User'
 
